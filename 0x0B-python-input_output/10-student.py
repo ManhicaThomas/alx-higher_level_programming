dbcbcfdf15dbc1dc8ab9d87d38ef81957f143697
ManhicaThomas <thomasmanchica@gmail.com>
2024-02-06 23:52:25 -0800

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,16 +9,16 @@ class Student:
         """Initialize a new Student.
 
         Args:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
-            """Get a dictionary representation of the Student.
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
 
         If attrs is a list of strings, represents only those attributes
         included in the list.
